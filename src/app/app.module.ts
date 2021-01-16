23fcb8c448b96a47095c93bdb91c19ca8de333e8
patorma <pocontreras@ing.ucsc.cl>
2021-01-16 18:18:12 -0300

    configuracion inicial de rutas del proyecto y agregacion de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoticiaCompletaComponent } from './components/noticia-completa/noticia-completa.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticiaCompletaComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
